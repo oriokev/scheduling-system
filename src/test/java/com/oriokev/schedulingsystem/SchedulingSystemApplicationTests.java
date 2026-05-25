e2c899522c1f1986eb7ecb7e951054d28308472e
Ori Okev <orio@supplant.me>
2026-05-25 15:16:49 +0300

    Initial commit

---
+++ src/test/java/com/oriokev/schedulingsystem/SchedulingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oriokev.schedulingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
